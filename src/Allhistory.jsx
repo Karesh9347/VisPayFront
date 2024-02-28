a130ace05e786ec5f033594967c5490143f5732e
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-02-29 01:03:10 +0530

    Add files via upload

---
+++ src/Allhistory.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { ListGroup, ListGroupItem,Table } from 'react-bootstrap';
+
+const Allhistory = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/alltransactions');
+        setTransactions(response.data);
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Transactions</h1>
+      {transactions && transactions.length > 0 ? (
+        <Table responsive striped bordered hover className="custom-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Roll Number</th>
+              <th>Mobile Number</th>
+              <th>Transactions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.rollNumber}</td>
+                <td>{user.mobileNumber}</td>
+                <td>
+                  <Table responsive striped bordered hover>
+                  <thead>
+                          <tr>
+                            <th>transaction id</th>
+                            <th>amount</th>
+                            <th>date</th>
+                          </tr>
+                        </thead>
+                    {user.transactions.map((transaction, tIndex) => (
+                      
+                        
+                        <tbody key={tIndex}>
+                          <td>{transaction.transactionId}</td>
+                          <td>{transaction.amount}</td>
+                          <td>{transaction.date.slice(0,10)}</td>
+                        </tbody>
+                    
+                       
+                     
+                    ))}
+                     </Table>
+               
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <p>No transactions available</p>
+      )}
+    </div>
+  );
+};
+
+export default Allhistory;
---
+++ src/Deletes.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Nav from '../Nav'
+import BottomNavbar from '../Bottom'
+import { Container,Form,Button,FormControl,FormLabel, FormGroup,Alert} from 'react-bootstrap';
+import '../App.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Deletes = () => {
+  const [rollNumber, setRollNumber] = useState("");
+
+  const handleDelete = async () => {
+    try {
+      if(rollNumber.length!==10){
+        toast("roll number must and should contain 10 chars")
+       
+      }
+      else{
+
+     
+      const response = await axios.delete('http://localhost:3001/deleteStudent', {
+        data: { rollNumber },
+      });
+
+       toast(response.data.message);
+    }
+      
+    } catch (error) {
+      console.error(error.message);
+      toast("Error deleting student");
+    }
+  
+  };
+  
+
+  return (
+    <div style={{marginTop:"100px"}}>
+      <Nav/>
+      <Container id="subm" className="card" style={{width:"340px"}}> 
+      <center className='card-header'>
+      <h2 id="heading">Delete Student</h2>
+     </center>
+     <div>
+      <Form>
+        <FormGroup>
+      <FormLabel>
+        Roll Number:</FormLabel>
+        <FormControl type="text" value={rollNumber} onChange={(e) => setRollNumber(e.target.value)} placeholder='enter student rollNumber to delete'/> 
+        </FormGroup><br/>
+        <FormGroup>
+      
+      <Button onClick={handleDelete} className='bg-danger' style={{width:"300px"}}>Delete Student</Button>
+      
+        <ToastContainer />
+      </FormGroup>
+      
+      </Form>
+      </div>
+      </Container>
+    
+      <BottomNavbar/>
+    </div>
+  );
+};
+
+export default Deletes;
