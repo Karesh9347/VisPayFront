1db748adef6915e4819860855d05ef39c0f6fa79
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-03-01 02:59:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
-
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PaymentForm from './PaymentForm';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Success from './Success'
+import Success from './Success';
 import Failure from './Failure';
 import Profile from './Profile';
 import History from './History';
 import Login from './Login';
 import Registration from './Registeration';
 import TermsAndConditions from './Terms';
-import Allhistory from './Allhistory';
-import Deletes from './Deletes';
+import Allhistory from './Admin/Allhistory';
+import Deletes from './Admin/Deletes';
+import Doc from './Doc';
+import A404 from './404';
+
 export const store = createContext();
 
 function App() {
   
-  const [token, setToken] = useState(null);
-  const [category, setCategory] = useState("college fee");
+  const token=localStorage.getItem("token")
+
+
   return (
-    <store.Provider value={{ token, setToken,}}>
+    <store.Provider value={{ token}}>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/payment"
-            element={
-              <PaymentForm 
-              category={category}
-              setCategory={setCategory}
-              />
-            }
-          ></Route>
-
-          <Route path="/success" element={<Success category={category}
-              setCategory={setCategory} />}></Route>
-          <Route path="/failure" element={<Failure />}></Route>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/history" element={<History />} />
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Registration />} />
-          <Route path="/terms" element={<TermsAndConditions/>}/>
-          <Route path="/allTransactions" element={<Allhistory/>}/>
-          <Route path="/studentDelete" element={<Deletes/>}/>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/documentation" element={<Doc />} />
+          {token ? (
+            <>
+              <Route
+                path="/success"
+                element={<Success />}
+              />
+              <Route path="/failure" element={<Failure />} />
+           
+              <Route path="/history" element={<History />} />
+              <Route path="/terms" element={<TermsAndConditions />} />
+              <Route path="/allTransactions" element={<Allhistory />} />
+              <Route path="/studentDelete" element={<Deletes />} />
+             
+              <Route
+                path="/payment"
+                element={<PaymentForm />}
+              />
+            </>
+          ) : null}
         </Routes>
-      </BrowserRouter >
+      </BrowserRouter>
     </store.Provider>
-
   );
 }
 
