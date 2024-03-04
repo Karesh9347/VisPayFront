c7b8c437193deb8c8346be70fc3aa1bd703cff76
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-03-04 12:05:44 +0530

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,6 +1,6 @@
 
 import { Card, Col, Row,Table } from 'react-bootstrap';
-import {useEffect,useReducer,useState} from 'react';
+import {useEffect,useReducer,useState,useRef} from 'react';
 import './Admin.css';
 import Login from './Login';
 import AdminNav from './AdminNav';
@@ -8,7 +8,10 @@ import Container from 'react-bootstrap/Container';
 import axios from 'axios';
 import { base_url } from './key';
 import './Admin.css'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 function Admin() {
+     const profileRef=useRef();
     const [transactions, setTransactions] = useState([]);
  const storedToken=localStorage.getItem("token")
  const [totalTransactions,setTotalTransactions]=useState();
@@ -37,6 +40,26 @@ function Admin() {
 
   fetchTransactions();
 }, []);
+    const pdfDownload=async()=>{
+  const contentRef = profileRef.current;
+
+try {
+  if (contentRef) {
+    const canvas = await html2canvas(contentRef, { scale: 1 });
+    const imageData = canvas.toDataURL('image/png');
+    const pdf = new jsPDF({
+      orientation: 'landscape',
+      unit: 'mm',
+      format: 'a4',
+    });
+    pdf.addImage(imageData, 'PNG', 0, 0, 290, 210);
+    pdf.save('all_user.pdf');
+  }
+} catch (err) {
+  console.error(err);
+}
+
+}
   return (
     <>
     {storedToken?(
@@ -76,12 +99,15 @@ function Admin() {
         <Row>
           
           <Col >
-          <div className='heading-container'>
+        <div className='heading-container' style={{display:"flex",justifyContent:"space-evenly"}}>
           <h1 className='heading'>All User List</h1>
+          <div>
+         <Button onClick={pdfDownload}>download pdf</Button>
+        
+        </div>
           </div>
-      
           {transactions && transactions.length > 0 ? (
-        <Table responsive striped bordered hover className="custom-table">
+        <Table responsive striped bordered hover className="custom-table" ref={profileRef}>
           <thead>
             <tr>
               <th>Name</th>
